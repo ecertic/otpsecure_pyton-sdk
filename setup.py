ce87693928cdcc8bb50570c839a53ab393d25119
ecertic <sistemas@ecertic.com>
2016-10-12 19:51:12 +0000

    v1.0.4 encoding issues

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
 	url				 = 'https://github.com/ecertic/otpsecure_pyton-sdk',
 	download_url	 = 'https://github.com/ecertic/otpsecure_pyton-sdk/tarball/1.0.4',
 	keywords		 = ['otpsecure', 'sms'],
-	install_requires = ['requests>=2.4.1','pycrypto>=2.6.1'],
+	install_requires = ['requests>=2.4.1','pycrypto>=2.6.1','beautifulsoup4'],
 	license			 = 'BSD-2-Clause',
 )
